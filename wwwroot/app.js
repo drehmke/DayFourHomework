8d0eee95ad7c62e2387b753d029e7f6e16aff0dc
Donna Ryan <donna@nerdrose.com>
2016-10-20 21:57:26 -0700

    Its probably not the most elegant solution, but it works

---
+++ wwwroot/app.js
@@ -0,0 +1,95 @@
+var __extends = (this && this.__extends) || function (d, b) {
+    for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p];
+    function __() { this.constructor = d; }
+    d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+};
+// Declare Parent class - I want it to implement IProduct so that I know I have these properties
+var Product = (function () {
+    function Product(name, price) {
+        this.name = name;
+        this.price = price;
+    }
+    return Product;
+}());
+// Declare Children classes - because these are unique
+var Cat = (function (_super) {
+    __extends(Cat, _super);
+    function Cat(name, price, age) {
+        _super.call(this, name, price);
+        this.age = age;
+        this.age = age;
+    }
+    return Cat;
+}(Product));
+var Dog = (function (_super) {
+    __extends(Dog, _super);
+    function Dog(name, price, canFetch, earLength) {
+        _super.call(this, name, price);
+        this.canFetch = canFetch;
+        this.earLength = earLength;
+        this.canFetch = canFetch;
+        this.earLength = earLength;
+    }
+    return Dog;
+}(Product));
+var Toy = (function (_super) {
+    __extends(Toy, _super);
+    function Toy(name, price, material) {
+        _super.call(this, name, price);
+        this.material = material;
+        this.material = material;
+    }
+    return Toy;
+}(Product));
+// Create my products - 6 cats, 4 dogs, 3 toys
+var cat1 = new Cat("Matt", 16.57, 13);
+var cat2 = new Cat("Pookie", 25.56, 9);
+var cat3 = new Cat("Loki", 25.63, 3);
+var cat4 = new Cat("Fluffy", 65.45, 1);
+var cat5 = new Cat("Mittens", 78.98, .5);
+var cat6 = new Cat("Shadow", 15.36, 20);
+var dog1 = new Dog("Barkspawn", 96.38, true, "short");
+var dog2 = new Dog("Ralph", 25.01, false, "long");
+var dog3 = new Dog("Boy", 75.54, true, "long");
+var dog4 = new Dog("Princess", 43.86, false, "short");
+var toy1 = new Toy("Pull Toy", 2.59, "string");
+var toy2 = new Toy("Squeaky Newspaper", 3.65, "rubber");
+var toy3 = new Toy("Laser Pointer", 5.36, "metal");
+// Setup my array of products - they should all use the IProduct interface
+var inventory = [cat1, cat2, cat3, cat4, cat5, cat6, dog1, dog2, dog3, dog4, toy1, toy2, toy3];
+// Fun time!
+var btnAll = document.getElementById('all');
+var btnCat = document.getElementById('cats');
+var btnDog = document.getElementById('dogs');
+function showProductDetails(products, display) {
+    if (display === void 0) { display = "all"; }
+    for (var _i = 0, products_1 = products; _i < products_1.length; _i++) {
+        var prod = products_1[_i];
+        var typeThis = void 0;
+        var txtDisplay = void 0;
+        if (prod.age) {
+            typeThis = "cat";
+        }
+        else if (prod.earLength) {
+            typeThis = "dog";
+        }
+        else if (prod.material) {
+            typeThis = "toy";
+        }
+        if (display == "cat" && typeThis == "cat") {
+            txtDisplay = "The " + typeThis + " named " + prod.name + " will cost you $" + prod.price + ".";
+        }
+        else if (display == "dog" && typeThis == "dog") {
+            txtDisplay = "The " + typeThis + " named " + prod.name + " will cost you $" + prod.price + ".";
+        }
+        else if (display == "all") {
+            txtDisplay = "The " + typeThis + " " + prod.name + " will cost you $" + prod.price + ".";
+        }
+        if (txtDisplay != undefined) {
+            console.log(txtDisplay);
+        }
+    }
+}
+btnAll.addEventListener('click', function () { showProductDetails(inventory, "all"); });
+btnCat.addEventListener("click", function () { showProductDetails(inventory, "cat"); });
+btnDog.addEventListener("click", function () { showProductDetails(inventory, "dog"); });
---
+++ wwwroot/app.ts
@@ -0,0 +1,79 @@
+﻿// Declare Interface
+interface IProduct {
+    name: string;
+    price: number;
+}
+// Declare Parent class - I want it to implement IProduct so that I know I have these properties
+abstract class Product implements IProduct {
+    constructor(public name: string, public price: number) {}
+}
+// Declare Children classes - because these are unique
+class Cat extends Product {
+    constructor(name: string, price: number, public age: number) {
+        super(name, price);
+        this.age = age;
+    }
+}
+
+class Dog extends Product {
+    constructor(name: string, price: number, public canFetch: boolean; public earLength: string) {
+        super(name, price);
+        this.canFetch = canFetch;
+        this.earLength = earLength;
+    }
+}
+
+class Toy extends Product {
+    constructor(name: string, price: number, public material: string) {
+        super(name, price);
+        this.material = material;
+    }
+}
+
+// Create my products - 6 cats, 4 dogs, 3 toys
+let cat1 = new Cat(`Matt`, 16.57, 13);
+let cat2 = new Cat(`Pookie`, 25.56, 9);
+let cat3 = new Cat(`Loki`, 25.63, 3);
+let cat4 = new Cat(`Fluffy`, 65.45, 1);
+let cat5 = new Cat(`Mittens`, 78.98, .5);
+let cat6 = new Cat(`Shadow`, 15.36, 20);
+let dog1 = new Dog(`Barkspawn`, 96.38, true, `short`);
+let dog2 = new Dog(`Ralph`, 25.01, false, `long`);
+let dog3 = new Dog(`Boy`, 75.54, true, `long`);
+let dog4 = new Dog(`Princess`, 43.86, false, `short`);
+let toy1 = new Toy(`Pull Toy`, 2.59, `string`);
+let toy2 = new Toy(`Squeaky Newspaper`, 3.65, `rubber`);
+let toy3 = new Toy(`Laser Pointer`, 5.36, `metal`);
+
+// Setup my array of products - they should all use the IProduct interface
+let inventory: IProduct[] = [cat1, cat2, cat3, cat4, cat5, cat6, dog1, dog2, dog3, dog4, toy1, toy2, toy3];
+
+// Fun time!
+let btnAll = <HTMLButtonElement>document.getElementById('all');
+let btnCat = <HTMLButtonElement>document.getElementById('cats');
+let btnDog = <HTMLButtonElement>document.getElementById('dogs');
+
+function showProductDetails(products: IProduct[], display = `all`) {
+    for (let prod of products) {
+        let typeThis: string;
+        let txtDisplay: string;
+
+        if (prod.age)               { typeThis = `cat`; }
+        else if (prod.earLength)    { typeThis = `dog`; }
+        else if (prod.material)     { typeThis = `toy`; }
+
+        if (display == `cat` && typeThis == `cat`) {
+            txtDisplay = `The ${typeThis} named ${prod.name} will cost you $${prod.price}.`;
+        } else if (display == `dog` && typeThis == `dog`) {
+            txtDisplay = `The ${typeThis} named ${prod.name} will cost you $${prod.price}.`;
+        } else if (display == `all`) {
+            txtDisplay = `The ${typeThis} ${prod.name} will cost you $${prod.price}.`;
+        }
+        if (txtDisplay != undefined) { console.log(txtDisplay); }
+    }
+}
+
+
+btnAll.addEventListener('click', () => { showProductDetails(inventory, `all`); });
+btnCat.addEventListener(`click`, () => { showProductDetails(inventory, `cat`); });
+btnDog.addEventListener(`click`, () => { showProductDetails(inventory, `dog` });
